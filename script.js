c1b28c59ace1b062eee823fe14f689134fb294ee
Tuakkiek <153929918+Tuakkiek@users.noreply.github.com>
2025-03-10 21:42:48 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Lấy nút "Xem thêm" và phần thông tin ẩn
-const showMoreBtn = document.getElementById("show-more-btn");
-const moreInfoSection = document.getElementById("more-info");
-
-// Gắn sự kiện click cho nút
-showMoreBtn.addEventListener("click", () => {
-  moreInfoSection.classList.toggle("hidden");
-  showMoreBtn.textContent = moreInfoSection.classList.contains("hidden") ? "Xem thêm" : "Ẩn bớt";
-});
